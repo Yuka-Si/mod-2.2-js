69da544cd0e2b89a40d7120937bb9f6524fa84ed
Yulia <sivonenkou@gmail.com>
2024-01-05 13:48:34 +0200

    add

---
+++ 01-basics.js
@@ -0,0 +1,14 @@
+const add = function (x, y) {
+
+    console.log(x);
+    console.log(y);
+    console.log('Виконується функція add');
+
+    return x + y;
+
+
+};
+const r1 = add(5, 3);
+console.log(r1);
+
+add(2, 3);
