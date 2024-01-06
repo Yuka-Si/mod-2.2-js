20376f351b58a9b2e6b9dd8e3851ceb7fd626596
Yulia <sivonenkou@gmail.com>
2024-01-06 02:08:35 +0200

    add

--- 01-basics.js
+++ 01-basics.js
@@ -1,14 +1,44 @@
-const add = function (x, y) {
+// const add = function (x, y) {
 
-    console.log(x);
-    console.log(y);
-    console.log('Виконується функція add');
+//     console.log(x);
+//     console.log(y);
+//     console.log('Виконується функція add');
 
-    return x + y;
+//     return x + y;
 
 
-};
-const r1 = add(5, 3);
-console.log(r1);
+// };
+// const r1 = add(5, 3);
+// console.log(r1);
 
-add(2, 3);
+// add(2, 3);
+
+// function slugify(title) {
+//     // Change code below this line
+//     let slug = title.toLowerCase().split(" ").join('-');
+//     console.log(slug);
+//     return slug;
+
+
+//     // Change code above this line
+// 
+// slugify("English for developer"
+function findLongestWord(string) {
+    let word = string.split(' ');
+    let longWord = word[0];
+    for (let i = 0; i < word.length; i += 1) {
+
+
+        if (word[i].length > longWord.length) {
+            longWord = word[i];
+        }
+
+    }
+    return longWord;
+    // Change code below this line
+
+
+
+    // Change code above this line
+}
+findLongestWord("The quick brown fox jumped over the lazy dog")
